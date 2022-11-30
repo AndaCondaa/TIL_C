d2319682ea949fc845955ed3f4654f0d997433e6
AndaCondaa <simon5229@naver.com>
2022-11-30 10:19:18 +0900

    1130

--- CodingTest/CodingTest1.cpp
+++ CodingTest/CodingTest1.cpp
@@ -8,11 +8,17 @@ int b[200004];
 int t[200004];
 int T;
 int N;
-bool op = false; // 참 : 이미 제외,  거짓: 아직 제외 X
+bool isExcept = false;		// 참 : 이미 제외,  거짓: 아직 제외 X    // isExcept
 int max;
+int direction;
+int startIdx;
 
-int left(int idx, int max, bool op);
-int right(int idx, int max, bool op);
+enum directionFlags {
+	left,
+	right
+};
+
+int Calculation(int idx, int max, int direction, bool isExcept);
 
 int main()
 {
@@ -20,36 +26,28 @@ int main()
 
 	for (int cnt = 0; cnt < T; cnt++) {
 		scanf("%d", &N);
-
-		//a = (int*)malloc(sizeof(int) * N);
-		//b = (int*)malloc(sizeof(int) * N);
-
-		//memset(a, 0, sizeof(int) * N);
-		//memset(b, 0, sizeof(int) * N);
-
 		for (int i = 0; i < N; i++) {
 			scanf("%d", &a[i]);
 		}
-
+		
 		if (N == 1) {
 			if (a[0] < 0) a[0] *= (-1);
 			t[cnt] = a[0];
-		}
+		} 
 		else {
 			for (int i = 0; i < N; i++) {
-				op = false;
+				isExcept = false;
+				startIdx = i;
+				max = a[i];
 				if (i == 0) {
-					b[i] = a[i] + right(1, 0, op);
-				}
-				else if (i == N - 1) {
-					b[i] = a[i] + left(i - 1, 0, op);
+					b[i] = a[i] + Calculation(i + 1, max, right, isExcept);
 				}
 				else {
-					int tmp_max = left(i - 1, 0, op);
-					int max = right(i + 1, tmp_max, op) + tmp_max;
-					b[i] = a[i] + max;
+					b[i] = a[i] + Calculation(i - 1, max, left, isExcept);
 				}
+				printf("%d 맥스 %d\n", i, b[i]);
 			}
+
 			int result = 0;
 			for (int i = 0; i < N; i++) {
 				if (b[i] < 0) b[i] *= (-1);
@@ -66,44 +64,22 @@ int main()
 	return 0;
 }
 
-int left(int idx, int max, bool op)
+int Calculation(int idx, int max, int direction, bool isExcept)
 {
+	//인덱스 0 경계
 	if (idx == 0) {
 		if (a[idx] >= 0) {
-			return a[idx];
+			max += a[idx];
+			if (startIdx == N - 1) return a[idx];
+			return a[idx] + Calculation(startIdx + 1, max, right, isExcept);
 		}
-		else if (a[idx] < 0) {
-			return 0;
-		}
-	}
-
-	//양수인 경우
-	if (a[idx] >= 0) {
-		return left(idx - 1, max + a[idx], op) + a[idx];
-	}
-	//음수인 경우
-	else if (a[idx] < 0) {
-		if (op) {	// 이미 제외 한 경우
-			int tmp1 = left(idx - 1, max + a[idx], op) + a[idx];
-			if (max < tmp1) {
-				return tmp1;
-			}
-			else if (max >= tmp1) {
-				return 0;
-			}
-		}
-		else if (!op) {		// 아직 제외하지 않은 경우
-			int tmp1 = left(idx - 1, max + a[idx], op) + a[idx];	//본인 제외 X
-			int tmp2 = left(idx - 1, max, false);
-			if (tmp1 >= tmp2) return tmp1;
-			else if (tmp1 < tmp2) return tmp2;
+		else if (a[idx] < 0) { //음수를 만나면 제외할지 말지 결정해야 하지만, 왼쪽 끝이기 때문에 건너 뜀.
+			if (startIdx == N - 1) return 0;
+			return 0 + Calculation(startIdx + 1, max, right, isExcept);
 		}
 	}
-}
-
-int right(int idx, int max, bool op)
-{
-	if (idx == N-1) {
+	//인덱스 오른쪽 끝 경계
+	else if (idx == N - 1) {
 		if (a[idx] >= 0) {
 			return a[idx];
 		}
@@ -112,26 +88,43 @@ int right(int idx, int max, bool op)
 		}
 	}
 
-	//양수인 경우
-	if (a[idx] >= 0) {
-		return right(idx + 1, max + a[idx], op) + a[idx];
-	}
-	//음수인 경우
-	else if (a[idx] < 0) {
-		if (op) {	// 이미 제외 한 경우
-			int tmp1 = right(idx + 1, max + a[idx], op) + a[idx];
-			if (max < tmp1) {
-				return tmp1;
+	switch (direction)
+	{
+	case left:
+		if (a[idx] >= 0) {
+			max += a[idx];
+			return a[idx] + Calculation(idx - 1, max, left, isExcept);
+		}
+		else if (a[idx] < 0) {
+			if (isExcept) {	//이미 제외 한 경우
+				int tmp = a[idx] + Calculation(idx - 1, max + a[idx], left, isExcept);
+				return max < tmp ? tmp : 0;
 			}
-			else if (max >= tmp1) {
-				return 0;
+			else if (!isExcept) { //아직 제외하지 않은 경우
+				// 현재 인덱스 값 제외 x
+				int tmp1 = a[idx] + Calculation(idx - 1, max, left, isExcept);
+				// 현재 인덱스 값 제외 o
+				int tmp2 = Calculation(idx - 1, max, left, true);
+				return tmp1 > tmp2 ? tmp1 : tmp2;
 			}
 		}
-		else if (!op) {		// 아직 제외하지 않은 경우
-			int tmp1 = right(idx + 1, max + a[idx], op) + a[idx];	//본인 제외 X
-			int tmp2 = right(idx + 1, max, false);
-			if (tmp1 >= tmp2) return tmp1;
-			else if (tmp1 < tmp2) return tmp2;
+	case right:
+		if (a[idx] >= 0) {
+			max += a[idx];
+			return a[idx] + Calculation(idx + 1, max, right, isExcept);
+		}
+		else if (a[idx] < 0) {
+			if (isExcept) {	//이미 제외 한 경우
+				int tmp = a[idx] + Calculation(idx + 1, max + a[idx], right, isExcept);
+				return max < tmp ? tmp : 0;
+			}
+			else if (!isExcept) { //아직 제외하지 않은 경우
+				// 현재 인덱스 값 제외 x
+				int tmp1 = a[idx] + Calculation(idx + 1, max, right, isExcept);
+				// 현재 인덱스 값 제외 o
+				int tmp2 = Calculation(idx + 1, max, right, true);
+				return tmp1 > tmp2 ? tmp1 : tmp2;
+			}
 		}
 	}
 }
